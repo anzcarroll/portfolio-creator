06d934cf4c57cc7a68323f435b5d9d79394c0ff1
anzcarroll <anzcarroll@gmail.com>
2017-07-27 15:02:57 -0400

    homepage route to projects created

---
+++ routes/projects.js
@@ -0,0 +1,62 @@
+//======================
+// REQUIREMENTS
+//======================
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+//======================
+// INDEX
+//======================
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
+//======================
+// NEW
+//======================
+// Create a GET new route "/new" that renders the new.hbs form
+
+
+
+//======================
+// SHOW
+//======================
+// Create a GET show route "/:id" that renders the donut's show page
+
+
+
+
+//======================
+// CREATE
+//======================
+
+
+
+
+//======================
+// EDIT
+//======================
+
+
+
+//======================
+// UPDATE
+//======================
+
+
+
+//======================
+// DELETE
+//======================
+
+
+
+
+//======================
+// EXPORTS
+//======================
+module.exports = router;
